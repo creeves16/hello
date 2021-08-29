edb8c884086d707e0265d5c7c049b153779869ad
Brian Downey <bdowney3@huskers.unl.edu>
2021-08-29 14:50:39 +0000

    added partner info

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Cathina Reeves!\n");
   printf("Civil Engineering minoring in Environmental Engineering\n");
+  printf("Brian Downey!\n");
+  printf("Civil Engineering\n");
 
   return 0;
 }
